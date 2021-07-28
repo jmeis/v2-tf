258ae52f28fee089286f278c60a821271cb38f81
Joseph Meis <jmeis@users.noreply.github.com>
2021-07-28 16:34:23 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,18 +15,6 @@ provider "ibm" {
   ibmcloud_api_key   = var.ibmcloud_api_key
 }
 
-resource "ibm_container_cluster" "cluster" {
-  name              = var.cluster_name
-  datacenter        = var.datacenter
-  default_pool_size = var.default_pool_size
-  machine_type      = var.machine_type
-  hardware          = var.hardware
-  kube_version      = var.kube_version
-  public_vlan_id    = var.public_vlan_num
-  private_vlan_id   = var.private_vlan_num
-  resource_group_id = data.ibm_resource_group.group.id
-}
-
 resource "ibm_resource_instance" "cloudant" {
   name              = "my-cloudant-db"
   service           = "cloudantnosqldb"
